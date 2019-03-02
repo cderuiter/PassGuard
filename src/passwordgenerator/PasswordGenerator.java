fd7d236c5410735ac58630a3f2b5f31403d664cc
CSCarver <47365870+CSCarver@users.noreply.github.com>
2019-03-02 17:03:57 -0500

    Update PasswordGenerator.java
    Now filters the input of "length" to force a minimum of 1 character.

--- src/passwordgenerator/PasswordGenerator.java
+++ src/passwordgenerator/PasswordGenerator.java
@@ -1,9 +1,9 @@
 /**
- * This code exists to generate random characters to be 
+ * This code exists to generate random characters to be
  * used as a password by the generatePassword function.
+ *
  * @author Lucas Carver
  */
-
 package passwordgenerator;
 
 import java.util.Random;
@@ -11,136 +11,153 @@ import java.util.Random;
 public class PasswordGenerator {
 
     static Random randomObject = new Random();
-    
+
     /**
      * Creating a string of selectable characters.
      */
-    static String uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    static String lowers = "abcdefghijklmnopqrstuvwxyz";
-    static String numbers = "0123456789";
-    static String specials = "@%+\\/'!#$^?:,(){}[]~`-_.";
-    static String selectables = uppers + lowers + numbers + specials;
-    
+    static final String UPPERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static final String LOWERS = "abcdefghijklmnopqrstuvwxyz";
+    static final String NUMBERS = "0123456789";
+    static final String SPECIALS = "@%+\\/'!#$^?:,(){}[]~`-_.";
+    static final String SELECTABLES = UPPERS + LOWERS + NUMBERS + SPECIALS;
+    static final int THEGOODLENGTH = 15;
+
     /**
      * Creates a password with the default length of 20 with special characters.
-     * @return                          Returns the password as a char array.
+     *
+     * @return Returns the password as a char array.
      */
     public static char[] PasswordGenerator() {
         return generatePassword();
     }
-    
+
     /**
-     * Creates a password with the specified length with or without special characters.
-     * @param length               The length of the password in characters.
-     * @param allowSpecial      To allow or disallow special characters.
-     * @return                          Returns the password as a char array.
+     * Creates a password with the specified length with or without special
+     * characters.
+     *
+     * @param length The length of the password in characters.
+     * @param allowSpecial To allow or disallow special characters.
+     * @return Returns the password as a char array.
      */
     public static char[] PasswordGenerator(int length, boolean allowSpecial) {
         return generatePassword(length, allowSpecial);
     }
-    
+
     /**
-     * Creates a password with the default length of 20 with or without special characters.
-     * @param allowSpecial      To allow or disallow special characters.
-     * @return                          Returns the password as a char array.
+     * Creates a password with the default length of 20 with or without special
+     * characters.
+     *
+     * @param allowSpecial To allow or disallow special characters.
+     * @return Returns the password as a char array.
      */
     public static char[] PasswordGenerator(boolean allowSpecial) {
         return generatePassword(allowSpecial);
     }
-    
+
     /**
      * Creates a password with specified length with special characters.
-     * @param length               The length of the password in characters.
-     * @return                          Returns the password as a char array.
+     *
+     * @param length The length of the password in characters.
+     * @return Returns the password as a char array.
      */
     public static char[] PasswordGenerator(int length) {
         return generatePassword(length);
     }
 
     /**
-     * Creates a password with the specified length with or without special characters.
-     * @param length               The length of the password in characters.
-     * @param allowSpecial      To allow or disallow special characters.
-     * @return                          Returns the password as a char array.
+     * Creates a password with the specified length with or without special
+     * characters.
+     *
+     * @param length The length of the password in characters.
+     * @param allowSpecial To allow or disallow special characters.
+     * @return Returns the password as a char array.
      */
     public static char[] generatePassword(int length, boolean allowSpecial) {
-
+        if (length <= 0) {
+            length = 1;
+        }
         char[] password = new char[length];
 
         if (allowSpecial == false) {
             for (int i = 0; i < length; i++) {
-                password[i] = selectables.charAt(randomObject.nextInt(62));
+                password[i] = SELECTABLES.charAt(randomObject.nextInt(62));
             }
         } else {
             for (int i = 0; i < length; i++) {
-                password[i] = selectables.charAt(randomObject.nextInt(86));
+                password[i] = SELECTABLES.charAt(randomObject.nextInt(86));
             }
         }
         return password;
     }
-    
+
     /**
-     * Creates a password with the default length of 20 with or without special characters.
-     * @param allowSpecial      To allow or disallow special characters.
-     * @return                          Returns the password as a char array.
+     * Creates a password with the default length of 20 with or without special
+     * characters.
+     *
+     * @param allowSpecial To allow or disallow special characters.
+     * @return Returns the password as a char array.
      */
     public static char[] generatePassword(boolean allowSpecial) {
-        int length = 20;
+        int length = THEGOODLENGTH;
 
         char[] password = new char[length];
 
         if (allowSpecial == false) {
             for (int i = 0; i < length; i++) {
-                password[i] = selectables.charAt(randomObject.nextInt(62));
+                password[i] = SELECTABLES.charAt(randomObject.nextInt(62));
             }
         } else {
             for (int i = 0; i < length; i++) {
-                password[i] = selectables.charAt(randomObject.nextInt(86));
+                password[i] = SELECTABLES.charAt(randomObject.nextInt(86));
             }
         }
         return password;
     }
-    
+
     /**
      * Creates a password with the specified length with special characters.
-     * @param length              The length of the password in characters.
-     * @return                         Returns the password as a char array.
+     *
+     * @param length The length of the password in characters.
+     * @return Returns the password as a char array.
      */
     public static char[] generatePassword(int length) {
-
+        if (length <= 0) {
+            length = 1;
+        }
         boolean allowSpecial = true;
 
         char[] password = new char[length];
 
         if (allowSpecial == false) {
             for (int i = 0; i < length; i++) {
-                password[i] = selectables.charAt(randomObject.nextInt(62));
+                password[i] = SELECTABLES.charAt(randomObject.nextInt(62));
             }
         } else {
             for (int i = 0; i < length; i++) {
-                password[i] = selectables.charAt(randomObject.nextInt(86));
+                password[i] = SELECTABLES.charAt(randomObject.nextInt(86));
             }
         }
         return password;
     }
-    
+
     /**
-     * Creates a password with default length 20 and special charaters.
-     * @return                      Returns the password a char array.
+     * Creates a password with default length 20 and special characters.
+     *
+     * @return Returns the password a char array.
      */
     public static char[] generatePassword() {
-        int length = 20;
+        int length = THEGOODLENGTH;
         boolean allowSpecial = true;
 
         char[] password = new char[length];
 
         if (allowSpecial == false) {
             for (int i = 0; i < length; i++) {
-                password[i] = selectables.charAt(randomObject.nextInt(62));
+                password[i] = SELECTABLES.charAt(randomObject.nextInt(62));
             }
         } else {
             for (int i = 0; i < length; i++) {
-                password[i] = selectables.charAt(randomObject.nextInt(86));
+                password[i] = SELECTABLES.charAt(randomObject.nextInt(86));
             }
         }
         return password;
